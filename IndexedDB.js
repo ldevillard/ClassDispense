5adcf54d5c39eae7b3d2d5d9e6e4a26d3ddd6f2f
ldevillard <71450527+ldevillard@users.noreply.github.com>
2025-09-21 15:03:25 +0000

    Deploying to gh-pages from @ ldevillard/ClassDispense@dba5ca6dff4c75a0223cead240999c78de5522e4 🚀

---
+++ IndexedDB.js
@@ -0,0 +1,39 @@
+﻿const DB_NAME = "PupilDB";
+const DB_VERSION = 1;
+const STORE_NAME = "pupils";
+
+async function openDB() {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open(DB_NAME, DB_VERSION);
+        request.onupgradeneeded = (event) => {
+            const db = event.target.result;
+            if (!db.objectStoreNames.contains(STORE_NAME)) {
+                db.createObjectStore(STORE_NAME, { keyPath: "Id", autoIncrement: true });
+            }
+        };
+        request.onsuccess = (event) => resolve(event.target.result);
+        request.onerror = (event) => reject(event.target.error);
+    });
+}
+
+window.savePupils = async (pupils) => {
+    const db = await openDB();
+    const tx = db.transaction(STORE_NAME, "readwrite");
+    const store = tx.objectStore(STORE_NAME);
+    await store.clear();
+    for (const pupil of pupils) {
+        store.add(pupil);
+    }
+    return tx.complete;
+};
+
+window.loadPupils = async () => {
+    const db = await openDB();
+    return new Promise((resolve, reject) => {
+        const tx = db.transaction(STORE_NAME, "readonly");
+        const store = tx.objectStore(STORE_NAME);
+        const request = store.getAll();
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+    });
+};
