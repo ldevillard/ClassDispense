fa224495fa041030b4d80b04470e527e75972eb0
logandev <logandvllrd@gmail.com>
2025-09-22 22:35:47 +0200

    added navmenu with dark/light theme switcher

---
+++ Layout/NavMenu.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ClassDispense.Layout
+{
+    public partial class NavMenu : ComponentBase
+    {
+        bool drawerOpen = false;
+        bool isDarkMode = true;
+
+        void ToggleNavMenu()
+        {
+            drawerOpen = !drawerOpen;
+        }
+
+        void ToggleDarkLight()
+        {
+            isDarkMode = !isDarkMode;
+        }
+    }
+}
